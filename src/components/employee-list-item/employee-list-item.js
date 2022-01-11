14737be826e1b82da45f5cd590db489ecc7cb55d
RomaRoma <v12nes11@gmail.com>
2022-01-11 21:02:53 +0200

    [101] optimised

--- src/components/employee-list-item/employee-list-item.js
+++ src/components/employee-list-item/employee-list-item.js
@@ -2,7 +2,7 @@ import './employee-list-item.css';
 
 const EmployeeListItem = (props) => {
 
-    const { name, salary, onDelete, onToggleIncrease, onToggleRise, increase, rise } = props;
+    const { name, salary, onDelete, onToggleProp, increase, rise } = props;
     let classes = "list-group-item d-flex justify-content-between"
 
     if (increase === true) {
@@ -15,12 +15,14 @@ const EmployeeListItem = (props) => {
 
     return (
         <li className={classes}>
-            <span className="list-group-item-label"
-                onClick={onToggleRise}>{name}</span>
+            <span className="list-group-item-label" 
+                data-toggle="rise"
+                onClick={onToggleProp}>{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={`${salary}$`} />
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
-                    onClick={onToggleIncrease}
+                    data-toggle="increase"
+                    onClick={onToggleProp}
                     className="btn-cookie btn-sm ">
                     <i className="fas fa-cookie"></i>
                 </button>
