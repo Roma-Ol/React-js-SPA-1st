c8de479dba486646e70d921e25c81e29c1fdef26
RomaRoma <v12nes11@gmail.com>
2022-01-12 21:00:52 +0200

    [102] Added search

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,12 +1,31 @@
-import './search-panel.css'
+import {Component} from 'react';
+import './search-panel.css';
 
-const SearchPanel = () => {
-  return(
-    <input 
-      type='text'
-      className='form-control search-input'
-      placeholder='Find employee'/>
-  )
+class SearchPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term:  ''
+    }
+  }
+
+  onFormFill = (e) => {
+    const term = e.target.value;
+    this.setState({term})
+    this.props.onUpdateSearch(term);
+  }
+
+  render() {
+    return(
+      <input
+        onChange={this.onFormFill}
+        type='text'
+        className='form-control search-input'
+        placeholder='Find employee'
+        value={this.state.term}
+        />
+    )
+  }
 }
 
 export default SearchPanel;
