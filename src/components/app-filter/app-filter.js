e07baf3251640333498c94779e8d96875c789930
RomaRoma <v12nes11@gmail.com>
2022-01-23 22:34:20 +0200

    [102] Filter func

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,25 +1,45 @@
+import {Component} from "react";
 import './app-filter.css';
 
-const AppFilter = () => {
-  return(
-    <div className="btn-group">
-      <button 
-        className="btn btn-light"
-        type='button'>
-          Show all
-      </button>
-      <button 
-        className="btn btn-outline-light"
-        type='button'>
-          Need promotion
-      </button>
-      <button 
-        className="btn btn-outline-light"
-        type='button'>
-          1000$ and up
-      </button>
-    </div>
-  )
+class AppFilter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterCriteria: 'all'
+        }
+    }
+    onFilter = (e) => {
+        const filterCriteria = e.target.getAttribute('data-filter');
+        this.setState({filterCriteria});
+        this.props.onFilterSelect(filterCriteria);
+    }
+    render() {
+        return (
+            <div className="btn-group">
+                <button
+                    className="btn btn-light"
+                    type='button'
+                    data-filter='all'
+                    onClick={this.onFilter}>
+                    Show all
+                </button>
+                <button
+                    className="btn btn-outline-light"
+                    type='button'
+                    data-filter='toPromote'
+                    onClick={this.onFilter}>
+                    Need promotion
+                </button>
+                <button
+                    className="btn btn-outline-light"
+                    type='button'
+                    data-filter='1kPlus'
+                    onClick={this.onFilter}>
+                    1000$ and up
+                </button>
+            </div>
+        )
+    }
 }
 
 export default AppFilter;
